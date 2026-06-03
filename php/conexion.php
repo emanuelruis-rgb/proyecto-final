5dfb6c3f9cf449cff8211756012e260c2c824bbc
emanuelruis-rgb <emanuelruis1208@gmail.com>
2026-06-03 12:07:13 -0300

    cambio

--- php/conexion.php
+++ php/conexion.php
@@ -5,10 +5,4 @@ $clave = "";
 $basededatos = "liga"; 
 
 $conexion = new mysqli($servidor, $usuario, $clave, $basededatos);
-
-if ($conexion->connect_error) {
-    die("Conexión fallida: " . $conexion->connect_error);
-} else {
-    echo "Conexión exitosa a la base de datos";
-}
 ?>
